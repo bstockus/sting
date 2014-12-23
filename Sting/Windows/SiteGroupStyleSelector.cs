24f05f36da5ad512b52f8bbf721b8a264dc6900a
bstockus <bstockus@gmail.com>
2014-12-23 13:44:02 -0600

    Added SiteGroupStyleSelector class.

---
+++ Sting/Windows/SiteGroupStyleSelector.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Sting.Windows {
+
+    public class SiteGroupStyleSelector : StyleSelector {
+
+        public Style DefaultStyle { get; set; }
+
+        public Style SiteGroupStyle { get; set; }
+
+        public override Style SelectStyle(object item, DependencyObject container) {
+            GroupItem groupItem = (GroupItem)item;
+            if (groupItem.Name == "") {
+                return DefaultStyle;
+            } else {
+                return SiteGroupStyle;
+            }
+        }
+
+    }
+}
