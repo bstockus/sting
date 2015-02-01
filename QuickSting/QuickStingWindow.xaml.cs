955d39b98e2a0c88a6ff6e66945181b12a537692
bstockus <bstockus@gmail.com>
2015-02-01 16:35:33 -0600

    Further work on QuickSting

---
+++ QuickSting/QuickStingWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+
+namespace QuickSting {
+    /// <summary>
+    /// Interaction logic for QuickStingWindow.xaml
+    /// </summary>
+    public partial class QuickStingWindow : Window {
+
+        KeyboardHook hook = new KeyboardHook();
+
+        public QuickStingWindow() {
+            InitializeComponent();
+
+            hook.KeyPressed += new EventHandler<KeyPressedEventArgs>(hook_KeyPressed);
+            hook.RegisterHotKey(ModifierKeys.Alt | ModifierKeys.Win, Keys.P);
+
+        }
+
+        void hook_KeyPressed(object sender, KeyPressedEventArgs e) {
+            System.Diagnostics.Debug.WriteLine("hook_KeyPressed!");
+            this.ToggleVisibility();
+        }
+
+        private void Window_Activated(object sender, EventArgs e) {
+            System.Diagnostics.Debug.WriteLine("Window_Activated");
+            txtSearch.Focus();
+        }
+
+        private void Window_Deactivated(object sender, EventArgs e) {
+            System.Diagnostics.Debug.WriteLine("Window_Deactivated");
+            this.HideWindow();
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e) {
+            System.Diagnostics.Debug.WriteLine("btnClose_Click");
+            this.HideWindow();
+        }
+
+        private void HideWindow() {
+            this.Visibility = System.Windows.Visibility.Hidden;
+        }
+
+        private void ShowWindow() {
+            this.Activate();
+            this.Visibility = System.Windows.Visibility.Visible;
+        }
+
+        private void ToggleVisibility() {
+            if (this.Visibility == System.Windows.Visibility.Hidden) {
+                this.ShowWindow();
+            } else {
+                this.HideWindow();
+            }
+        }
+
+        private void txtSearch_KeyDown(object sender, System.Windows.Input.KeyEventArgs e) {
+            System.Diagnostics.Debug.WriteLine("txtSearch_KeyDown");
+            if (e.Key == Key.Escape) {
+                this.HideWindow();
+                e.Handled = true;
+            } else if (e.Key == Key.Enter) {
+                e.Handled = true;
+            }
+        }
+
+    }
+}
--- QuickSting/Win32Interop.cs
+++ QuickSting/Win32Interop.cs
@@ -14,5 +14,12 @@ namespace QuickSting {
         [DllImportAttribute("user32.dll")]
         public static extern bool ReleaseCapture();
 
+        [DllImport("user32.dll")]
+        public static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
+
+
+        [DllImport("user32.dll")]
+        public static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
     }
 }
